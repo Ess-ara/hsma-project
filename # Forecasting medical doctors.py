ae345f8ba864f2000c0958918c499488bdeb4ab3
ess-ara <sfundu@hotmail.com>
2025-02-16 15:56:05 +0000

    Actual vs forecast

--- # Forecasting medical doctors.py
+++ # Forecasting medical doctors.py
@@ -1,5 +1,7 @@
 #Forecasting medical doctors
-
+"""
+import libraries and data
+"""
 #import libraries
 import pandas as pd
 import numpy as np
@@ -11,20 +13,21 @@ import warnings
 warnings.filterwarnings("ignore")
 
 #import data
-df = pd.read_csv("Member data.csv",index_col=0)
-df
-df.dtypes
+df = pd.read_csv("Member data.csv",
+                 index_col=[0],
+                 parse_dates=[0])
+print(df)
 
 #plot data onto graph
 color_pal = sns.color_palette()
-df.plot(#style='.',
+df.plot(#style='-',
         figsize=(10, 5),
         color=color_pal[0],
-        title="Doctors")
+        title="Consultants")
 plt.show()
 
-##Time series features - Train/test split
-split_date= "01/07/2024"
+#Time series features - Train/test split
+split_date= "2024-05-01"
 df_train =df.loc[df.index <= split_date].copy()
 df_test = df.loc[df.index > split_date].copy()
 
@@ -33,10 +36,15 @@ df_test \
     .rename(columns={"Fellows": "Test set"}) \
     .join(df_train.rename(columns={"Fellows": "Train set"}),
           how='outer') \
-    .plot(figsize=(10, 5), title='Doctors', ms=10)
+    .plot(figsize=(10, 5), title='Doctors (test & train)', ms=10)
 plt.show()
 
-#Simple prophet model
+"""
+simple prophet model
+"""
+#print title
+print("Simple prophet model")
+
 #format the data for prophet model (ds and y)
 df_train_prophet = df_train.reset_index() \
     .rename(columns={"Date":"ds",
@@ -45,7 +53,7 @@ df_train_prophet = df_train.reset_index() \
 #check that the columns names hve chnaged and have been formatted
 df_train_prophet.head()
 
-#apply and train the model
+#apply and train the model - CPU etc as ouput
 %time
 model = Prophet()
 model.fit(df_train_prophet)
@@ -61,19 +69,19 @@ df_test_fcst = model.predict(df_test_prophet)
 #model output of predictions which includes trend
 df_test_fcst.head()
 
-#plot prophet forecast on gragh
-"""
-model will show upper and lower count that it believes
-is reasonable for forecast of thw future
-"""
+#plot prophet forecast on gragh- upper&lower reasonable future forecast range
 fig, ax = plt.subplots(figsize=(10, 5))
 fig = model.plot(df_test_fcst, ax=ax)
 ax.set_title('Prophet Forecast')
 plt.show()
 
-"""
-the model trends etc can also be analysed and so 
-we can plot this
-"""
+#analyse model trends etc can also be analysed
 fig = model.plot_components(df_test_fcst)
-plt.show()
+
+"""
+plot the actuals against forecast
+"""
+f, ax = plt.subplots(figsize=(10,5))
+ax.scatter(df_test.index, df_test["Fellows"], color="r")
+fig = model.plot(df_test_fcst, ax=ax)
+ax.set_title("Actual vs Forecast")
