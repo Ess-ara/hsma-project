a0158b81cbd350cea2762c8b5c2db7073f2d7566
ess-ara <sfundu@hotmail.com>
2025-04-04 17:38:01 +0100

    Amend time series date

--- # Forecasting medical doctors - Copy.py
+++ # Forecasting medical doctors - Copy.py
@@ -27,7 +27,7 @@ df.plot(#style='-',
 plt.show()
 
 #Time series features - Train/test split
-split_date= "2024-05-01"
+split_date= "01-06-2024"
 df_train =df.loc[df.index <= split_date].copy()
 df_test = df.loc[df.index > split_date].copy()
 
